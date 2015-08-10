be5832dbf718c330d772c65e2beb250f40828ccf
Maurício Porfírio <mauriciosporfirio@gmail.com>
2015-08-10 10:16:35 -0300

    Mascaramento
    Retirada temporária do mascaremento

--- pages/pglogin.php
+++ pages/pglogin.php
@@ -6,40 +6,38 @@
         <base href="<?php echo RAIZ; ?>">
         <script src="js/jquery-2.1.4.min.js"></script>
         <script src="js/login.js"></script>
-        <script src="js/mascaras.js"></script>
-        <script src="js/jquery.mask.js"></script>
-        <link rel="stylesheet" type="text/css" href="css/login.css">
-        <link rel="shortcut icon" href="imagens/icon.ico" type="image/gif">
+        <!--<script src="js/mascaras.js"></script>-->
+        <!--<script src="js/jquery.mask.js"></script>-->
+        <link rel="stylesheet" type="text/css" media="all" href="css/login.css"/>
+        <link rel="shortcut icon" href="imagens/icon.ico" type="image/gif"/>
     </head>
     <body>
         <input type="hidden" value="<?php echo RAIZ; ?>" id="raiz" />
 
         <div id="fundoAzul">
             <div id="login">
-                <div id="login">
-                    <div id="imagens">
-                        <img id="computador" src="imagens/computador1.jpg">
-                        <a id="bola" class="slider" href="1">
-                            <img id="bola1" src="imagens/bolaSelecionada.png">
-                        </a>
-                        <a id="bolaSelecionada" class="slider" href="2">
-                            <img id="bola2" src="imagens/bola.png">
-                        </a>
-                    </div>
-                    <form id="areaLogin" name="login">
-                        <h1>Portal Aluno</h1>
-                        <div id="inputs">
-                            <input type="text" name="usuario" class="cpf" id="id"  required="required" maxlength="14" placeholder="CPF" >
-                            <br />
-                            <input type="password" name="senha" class="log" id="pass" required="required"  maxlength="20" placeholder="Senha">
-                            <br />
-                            <p id="info"></p>
-                        </div>
-                        <p id="checkbox"><input type="checkbox" id="box"> Mantenha-me Conectado</p>
-                        <input  value="Entrar" type="submit" id="button" name="button">
-                        <p id="esqueceuSenha">Esqueceu sua senha? <a href="recuperaSenha">Clique aqui</a></p>
-                    </form>
+                <div id="imagens">
+                    <img id="computador" src="imagens/computador1.jpg">
+                    <a id="bola" class="slider" href="1">
+                        <img id="bola1" src="imagens/bolaSelecionada.png">
+                    </a>
+                    <a id="bolaSelecionada" class="slider" href="2">
+                        <img id="bola2" src="imagens/bola.png">
+                    </a>
                 </div>
+                <form id="areaLogin" name="login">
+                    <h1>Portal Aluno</h1>
+                    <div id="inputs">
+                        <input type="text" name="usuario" class="cpf" id="id"  required="required" maxlength="14" placeholder="CPF" >
+                        <br />
+                        <input type="password" name="senha" class="log" id="pass" required="required"  maxlength="20" placeholder="Senha">
+                        <br />
+                        <p id="info"></p>
+                    </div>
+                    <p id="checkbox"><input type="checkbox" id="box"> Mantenha-me Conectado</p>
+                    <input  value="Entrar" type="submit" id="button" name="button">
+                    <p id="esqueceuSenha">Esqueceu sua senha? <a href="recuperaSenha">Clique aqui</a></p>
+                </form>
             </div>
         </div>
     </body>
