3bd19bfded1f6a7da934d62525b3b0bc5513b719
Naldo Maranhão <regisousa7@gmail.com>
2015-08-18 17:38:54 -0300

    AlteraDados e AlteraSenha
    deixando responsivo alteraDados e alteraSenha
    por: leo e naldo

--- pages/pgalterarSenha.php
+++ pages/pgalterarSenha.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="css/alterarDados.css">
 <form class="cadastro" id="simples">
     <div class="camposFormularioSimples">
         <div class="inputs">
@@ -5,20 +6,20 @@
 
             <div>
                 <label for="senhaAtual">Senha atual:</label><br/>
-                <input type="password" name="senhaAtual" />
+                <input type="password" name="senhaAtual" required="required" />
             </div>
 
             <div>
                 <label for="novaSenha">Nova senha:</label><br/>
-                <input type="password" name="novaSenha" />
+                <input type="password" name="novaSenha" required="required" />
             </div>
 
             <div>
                 <label for="confirmarSenha">Confirmar senha:</label><br/>
-                <input type="password" name="confirmarSenha" />
+                <input type="password" name="confirmarSenha" required="required" />
             </div>
 
-            <div>
+            <div id="botao">
                 <input name="enviar" class="alterar" type="submit" value="Alterar" />
             </div>
 
