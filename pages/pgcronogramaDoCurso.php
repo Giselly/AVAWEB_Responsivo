6feb51e32456a89d36ba9df37c91a379a044239e
Emesson <emesson.rede10@gmail.com>
2015-09-09 17:01:33 -0300

    Cronograma 2.0

--- pages/pgcronogramaDoCurso.php
+++ pages/pgcronogramaDoCurso.php
@@ -1,32 +1,80 @@
 <?php
-
 echo "Cronograma do Curso!";
 ?>
+<script src="../js/cronogramaDoCurso.js">
+</script>
 <div id="todo">
     <div id="tabela">
         <table id="cronograma">
-            <tr id="tr1">
-                <th>Capítulo 01 - COMUNICANDO IDEIAS</th>
-                <th>Capítulo 02 - PRINCÍPIOS DE DESIGN - Epifania da iúca</th>
-                <th>Capítulo 03 - PRINCÍPIOS DE DESIGN – Proximidade</th>
-                <th>Capítulo 04 - PRINCÍPIOS DE DESIGN – Alinhamento</th>
-                <th>Capítulo 05 - PRINCÍPIOS DE DESIGN – Repetição</th>
-                <th>Capítulo 06 - PRINCÍPIOS DE DESIGN - Contraste</th>
-                <th>Capítulo 07 - PRINCÍPIOS DE DESIGN – Tipo (& Vida)</th>
-                <th>Capítulo 08 - PRINCÍPIOS DE DESIGN – Categoria de tipos</th>
-                <th>Capítulo 09 - PRINCÍPIOS DE DESIGN – Contraste de tipos</th>
-                <th>Capítulo 10 - ROTEIRO</th>
-                <th>Capítulo 11 - SIGNOS E FORMAÇÃO DE SIGNIFICADO</th>
-                <th>Capítulo 12 - O CONTEXTO DA COR</th>
-                <th>Capítulo 13 - A TEORIA DA COR</th>
-                <th>Capítulo 14 - OS SISTEMAS DE COR</th>
-                <th>Capítulo 15 - A COR DIGITAL</th>
-                <th>Capítulo 16 - SLIDES: A CRIAÇÃO VISUAL</th>
-                <th>Capítulo 17 - SLIDES: A IDENTIDADE VISUAL</th>
-                <th>Capítulo 18 - O APRESENTADOR</th>
-                <th>Capítulo 19 - O POWERPOINT E OUTRAS FERRAMENTAS DE APOIO</th>
-                <th>Capítulo 20 - FECHAMENTO</th>
-            </tr>
+            <tbody>
+                <tr id="tr1">
+                    <th>Conteudo</th>
+                    <th>Data</th>
+                    <th>Visualizar</th>
+                </tr>
+
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 01 - COMUNICANDO IDEIAS</th>
+                    <td id="panel1" class="panel">1.1 A foLha de papel em branco</td>
+                    <td id="panel" class="panel">1.2 Por que ideias?</td>
+                    <td id="panel" class="panel">1.3 O briefing</td>
+                    <td id="panel" class="panel">1.4 Investigacao do assunto</td>
+                    <td id="panel" class="panel">1.5 Reunindo inspiracao</td>
+                    <td id="panel" class="panel">1.6 Brainstorming</td>
+                    
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 02 - PRINCIPIOS DE DESIGN - EPIFANIA UNICA</th>
+                    <td id="panel" class="panel">2.1 Os quatro principios basicos</td>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 03 - PRINCIPIOS DE DESIGN - PROXIMIDADE</th>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 04 - PRINCIPIOS DE DESIGN - ALINHAMENTO</th>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 05 - PRINCIPIOS DE DESIGN - REPETICAO</th>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 06 - PRINCIPIOS DE DESIGN - CONTRASTE</th>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 07 - PRINCIPIOS DE DESIGN - TIPO (& VIDA)</th>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 08 - PRINCIPIOS DE DESIGN - CATEGORIA E TIPOS</th>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 09 - PRINCIPIOS DE DESIGN - CONTRASTE E TIPOS</th>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 10 - ROTEIRO</th>
+                    <td id="panel" class="panel">10.1 Roteiro: O primeiro passo para uma boa apresentacao</td>
+                    <td id="panel" class="panel">10.2 Diagnostico: A base do roteiro</td>
+                    <td id="panel" class="panel">10.3 Redigindo o roteiro</td>
+                    <td id="panel" class="panel">10.4 A redacao de um roteiro inclui algumas etapas importantes</td>
+                    <td id="panel" class="panel">10.5 Estrategias para elevar a atencao da audiencia</td>
+                    <td id="panel" class="panel">10.6 As partes de uma apresentacao</td>
+                    <td id="panel" class="panel">10.7 Dez pontos a favor do seu roteiro</td>
+                </tr>
+                <tr class="conteudos">
+                    <th id="flip" class="flip">CAPITULO 11 - SIGNOS E FORMACAO DE SIGNIFICADOS</th>
+                    <td id="panel" class="panel">11.1 Introducao</td>
+                    <td id="panel" class="panel">11.2 Significante e significado</td>
+                    <td id="panel" class="panel">11.3 Signo</td>
+                    <td id="panel" class="panel">11.4 Icone</td>
+                    <td id="panel" class="panel">11.5 Indice</td>
+                    <td id="panel" class="panel">11.6 Simbolo</td>
+                    <td id="panel" class="panel">11.7 Emissor</td>
+                    <td id="panel" class="panel">11.8 Intencao</td>
+                    <td id="panel" class="panel">11.9 Mensagem</td>
+                    <td id="panel" class="panel">11.10 Transmissao</td>
+                    <td id="panel" class="panel">11.11 Ruido</td>
+                    <td id="panel" class="panel">11.12 Receptor</td>
+                    <td id="panel" class="panel">11.13 Destinacao</td>
+                </tr>
+            </tbody>
         </table>
     </div>
 </div>
